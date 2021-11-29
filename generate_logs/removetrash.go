61a9d1d513af76dec39e81a2853abbe77cf661de
Никита Рябченко <ryabchenko.n.a@topaz-atcs.com>
2021-11-29 20:04:33 +0300

    fix

--- generate_logs/removetrash.go
+++ generate_logs/removetrash.go
@@ -3,7 +3,9 @@ package generator
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"strings"
+	"time"
 
 	"gitlab.topaz-atcs.com/tmcs/logi2/logenc"
 )
@@ -30,4 +32,21 @@ func Remove(dirpath string, lineS string) {
 
 	}
 
+}
+
+func DeleteFile90(dir string) {
+	var cutoff = 24 * time.Hour * 70
+	fileInfo, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	now := time.Now()
+	fmt.Println(now)
+	for _, info := range fileInfo {
+		if diff := now.Sub(info.ModTime()); diff > cutoff {
+			fmt.Printf("Deleting %s which is %s old\n", info.Name(), diff)
+			logenc.DeleteOldsFiles(dir, info.Name(), "")
+
+		}
+	}
 }
