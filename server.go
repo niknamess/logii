90c00cf99014539418296f5aeea7c3cccf440494
Никита Рябченко <ryabchenko.n.a@topaz-atcs.com>
2022-01-13 17:30:39 +0300

    stable

--- server.go
+++ server.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+// требуется только ниже для обработки примера
+
+func Server() {
+
+	fmt.Println("Launching server...")
+
+	// Устанавливаем прослушивание порта
+	ln, _ := net.Listen("tcp", ":8081")
+
+	// Открываем порт
+	conn, _ := ln.Accept()
+
+	// Запускаем цикл
+	for {
+		// Будем прослушивать все сообщения разделенные \n
+		//message, _ := bufio.NewReader(conn).ReadString('\n')
+		// Распечатываем полученое сообщение
+		//fmt.Print("Message Received:", string(message))
+		// Процесс выборки для полученной строки
+		//newmessage := strings.ToUpper(message)
+		// Отправить новую строку обратно клиенту
+		//conn.Write([]byte(newmessage + "\n"))
+	}
+}
