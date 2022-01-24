820e918c2b1344b4d2dea63e92f893dfba1ff5fe
Никита Рябченко <ryabchenko.n.a@topaz-atcs.com>
2022-01-24 18:40:39 +0300

    fix

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,21 +1,15 @@
 angular.module("logi2").controller("mainController", mainController);
 
 mainController.$inject = ["$rootScope", "$scope", "$mdSidenav", "$http"]
-const button = document.getElementById('btn');
 const buttonR = document.getElementById('res');
+const buttonErr = document.getElementById('btnerr');
+const buttonInf = document.getElementById('btninf');
+const buttonDbgs = document.getElementById('btndbgs');
+const buttonWar = document.getElementById('btnwar');
+const buttonView = document.getElementById('view');
 
 var lastItem;
 //const input = document.querySelector('input');
-
-
-button.addEventListener('click', event => {
-    setTimeout(
-        () => {
-            initWS(lastItem)
-        },
-        1 * 200
-    );
-});
 
 buttonR.addEventListener('click', event => {
     setTimeout(
@@ -25,6 +19,85 @@ buttonR.addEventListener('click', event => {
         1 * 200
     );
 });
+
+buttonView.addEventListener('click', event => {
+    setTimeout(
+        () => {
+            //window.location.reload();
+            initWS(lastItem)
+            setBackColor('view', "#ed6c27")
+            quotation('view', "Buttom")
+        },
+        1 * 200
+    );
+});
+
+buttonErr.addEventListener('click', event => {
+    setTimeout(
+        () => {
+            initWSType(lastItem, "ERROR", "#ffb0b0")
+            setBackColor('view', "#ffb0b0")
+            quotation('view', "ERROR")
+        },
+        1 * 200
+    );
+});
+buttonInf.addEventListener('click', event => {
+    setTimeout(
+        () => {
+            initWSType(lastItem, "INFO", "#b0ffb0")
+            setBackColor('view', "#b0ffb0")
+            quotation('view', "INFO")
+        },
+        1 * 200
+    );
+});
+
+buttonDbgs.addEventListener('click', event => {
+    setTimeout(
+        () => {
+            initWSType(lastItem, "DEBUG", "#a0a0a0")
+            setBackColor('view', "#a0a0a0")
+            quotation('view', "DEBUG")
+        },
+        1 * 200
+    );
+});
+
+buttonWar.addEventListener('click', event => {
+    setTimeout(
+        () => {
+            initWSType(lastItem, "WARNING", "#ffff90")
+            setBackColor('view', "#ffff90")
+            quotation('view', "WARNING")
+                //setFontColor('view ', "black")
+        },
+        1 * 200
+    );
+});
+
+
+function setBackColor(btn, color) {
+    var property = document.getElementById(btn);
+    property.style.backgroundColor = color
+
+}
+
+function setFontColor(btn, color) {
+    var property = document.getElementById(btn);
+    property.style.color = color
+
+}
+
+function quotation(id, text) {
+    var q = document.getElementById(id);
+    if (q) q.innerHTML = text;
+}
+
+
+function change(identifier, color) {
+    identifier.style.background = color;
+}
 
 function mainController($rootScope, $scope, $mdSidenav, $http) {
 
@@ -87,9 +160,7 @@ function mainController($rootScope, $scope, $mdSidenav, $http) {
 }
 
 function initWS(file) {
-    // document.querySelector('button').removeEventListener(initWS(file));
 
-    //window.alert("InitWs Files" + file);
     var ws_proto = "ws:"
     if (window.location.protocol === "https:") {
         ws_proto = "wss:"
@@ -97,8 +168,7 @@ function initWS(file) {
 
     var socket = new WebSocket(ws_proto + "//" + window.location.hostname + ":" + window.location.port + "/ws/" + btoa(file));
     var container = angular.element(document.querySelector("#container"));
-    //var output = [],
-    //  i;
+
 
 
 
@@ -110,15 +180,17 @@ function initWS(file) {
         if (strf.indexOf("undefined") != 0) {
             container.append("<table class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%'> " +
                 "<col width=\"150px\" />" +
+                "<col width=\"150px\" />" +
                 "<col width=\"350px\" />" +
-                "<col width=\"110px\" />" +
+                "<col width=\"50px\" />" +
                 "<col width=\"130px\" />" +
-                "<col width=\"110px\" />" +
+                "<col width=\"100px\" />" +
                 "<col width=\"300px\" />" +
-                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
                 "<col width=\"500px\" />" +
                 "<col width=\"200px\" />" +
                 "<tr > <td>" +
+                "TYPE" + "</td> <td>" +
                 "APPNAME" + "</td> <td>" +
                 "APPPATH" + "</td> <td>" +
                 "APPPID" + "</td><td>" +
@@ -134,25 +206,17 @@ function initWS(file) {
         str = e.data.trim();
         if (str.indexOf("INFO") == 0) {
 
-            str = str.replace("INFO", "");
+            str = str.replace("INFO", ",INFO");
             // str = "<table  cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' >" +f2f3f4
             str = "<table  class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#b0ffb0\" >" +
-                /* "<col width=\"70%\" />" +
-                "<col width=\"70%\" />" +
-                "<col width=\"35%\" />" +
-                "<col width=\"35%\" />" +
-                "<col width=\"70%\" />" +
-                "<col width=\"100%\" />" +
-                "<col width=\"100%\" />" +
-                "<col width=\"0%\" />" +
-                "<col width=\"200px\" />" + */
+                "<col width=\"150px\" />" +
                 "<col width=\"150px\" />" +
                 "<col width=\"350px\" />" +
                 "<col width=\"50px\" />" +
                 "<col width=\"130px\" />" +
                 "<col width=\"100px\" />" +
                 "<col width=\"300px\" />" +
-                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
                 "<col width=\"500px\" />" +
                 "<col width=\"200px\" />" +
                 "<tr >" +
@@ -163,15 +227,16 @@ function initWS(file) {
             container.append(str);
 
         } else if (str.indexOf("ERROR") == 0) {
-            str = str.replace("ERROR", "");
+            str = str.replace("ERROR", ",ERROR");
             str = "<table class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#ffb0b0\" >" +
+                "<col width=\"150px\" />" +
                 "<col width=\"150px\" />" +
                 "<col width=\"350px\" />" +
                 "<col width=\"50px\" />" +
                 "<col width=\"130px\" />" +
                 "<col width=\"100px\" />" +
                 "<col width=\"300px\" />" +
-                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
                 "<col width=\"500px\" />" +
                 "<col width=\"200px\" />" +
                 "<tr >" +
@@ -182,16 +247,17 @@ function initWS(file) {
             container.append(str);
 
         } else if (str.indexOf("WARNING") == 0) {
-            str = str.replace("WARNING", "");
+            str = str.replace("WARNING", ",WARNING");
             // str = "<table  cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#ffcc00\" >" +
             str = "<table class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#ffff90\" >" +
+                "<col width=\"150px\" />" +
                 "<col width=\"150px\" />" +
                 "<col width=\"350px\" />" +
                 "<col width=\"50px\" />" +
                 "<col width=\"130px\" />" +
                 "<col width=\"100px\" />" +
                 "<col width=\"300px\" />" +
-                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
                 "<col width=\"500px\" />" +
                 "<col width=\"200px\" />" +
                 "<tr >" +
@@ -202,16 +268,17 @@ function initWS(file) {
             container.append(str);
             // container.append("<p style='background-color: yellow; color:blue'>" + str + "</p>" + "<hr>");
         } else if (str.indexOf("DEBUG") == 0) {
-            str = str.replace("DEBUG", "");
+            str = str.replace("DEBUG", ",DEBUG");
             // str = "<table cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#00ff00\" >" +
             str = "<table class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' bgcolor=\"#a0a0a0\" >" +
+                "<col width=\"150px\" />" +
                 "<col width=\"150px\" />" +
                 "<col width=\"350px\" />" +
                 "<col width=\"50px\" />" +
                 "<col width=\"130px\" />" +
                 "<col width=\"100px\" />" +
                 "<col width=\"300px\" />" +
-                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
                 "<col width=\"500px\" />" +
                 "<col width=\"200px\" />" +
                 "<tr >" +
@@ -221,7 +288,98 @@ function initWS(file) {
                 "</table>";
             container.append(str);
         } else {
-            container.append("<p style='background-color: yellow; color:blue'>" + str + "</p>" + "<hr>");
+            container.append("<p style='background-color: #ffff90; color:blue'>" + str + "</p>" + "<hr>");
+
+        }
+
+        //container.append(str + "<br>" + "<hr>");
+
+    }
+    socket.onclose = function() {
+        container.append("<p style='background-color: maroon; color:orange'>Connection Closed to WebSocket, tail stopped</p>");
+    }
+    socket.onerror = function(e) {
+        container.append("<b style='color:red'>Some error occurred " + e.data.trim() + "<b>");
+    }
+
+    return socket;
+}
+
+
+
+
+
+
+
+function initWSType(file, type, color) {
+
+    var ws_proto = "ws:"
+    if (window.location.protocol === "https:") {
+        ws_proto = "wss:"
+    }
+
+    var socket = new WebSocket(ws_proto + "//" + window.location.hostname + ":" + window.location.port + "/ws/" + btoa(file));
+    var container = angular.element(document.querySelector("#container"));
+
+
+
+
+    container.html("")
+    socket.onopen = function() {
+        var filename = file.replace(/^.*[\\\/]/, '')
+        container.append("<p><b>Tailing file: " + filename + "</b></p>");
+        strf = file
+        if (strf.indexOf("undefined") != 0) {
+            container.append("<table class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%'> " +
+                "<col width=\"150px\" />" +
+                "<col width=\"150px\" />" +
+                "<col width=\"350px\" />" +
+                "<col width=\"110px\" />" +
+                "<col width=\"130px\" />" +
+                "<col width=\"110px\" />" +
+                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
+                "<col width=\"500px\" />" +
+                "<col width=\"200px\" />" +
+                "<tr > <td>" +
+                "TYPE" + "</td> <td>" +
+                "APPNAME" + "</td> <td>" +
+                "APPPATH" + "</td> <td>" +
+                "APPPID" + "</td><td>" +
+                "THREAD" + "</td><td>" +
+                "TIME" + "</td><td>" +
+                "ULID" + "</td><td>" +
+                "MESSAGE" + "</td><td>" +
+                "DETAILS" + "</td></tr > </table >");
+        }
+    }
+
+    socket.onmessage = function(e) {
+        str = e.data.trim();
+        if (str.indexOf(type) == 0) {
+
+            str = str.replace(type, "");
+            // str = "<table  cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%' >" +f2f3f4
+            str = "<table  class=\"fixed\" cellspacing=\"0\" cellpadding=\"4\" border=\"1\" style='font-family:\"Courier New\", Courier, monospace; font-size:100%'" + "bgcolor=" + color + " >" +
+                "<col width=\"150px\" />" +
+                "<col width=\"150px\" />" +
+                "<col width=\"350px\" />" +
+                "<col width=\"50px\" />" +
+                "<col width=\"130px\" />" +
+                "<col width=\"100px\" />" +
+                "<col width=\"300px\" />" +
+                "<col width=\"400px\" />" +
+                "<col width=\"500px\" />" +
+                "<col width=\"200px\" />" +
+                "<tr >" +
+                str.replace(/,\n/g, "<tr >")
+                .replace(/,/g, "<td width=\"100\" height=\"100\">")
+                .replace(/<tr>$/, "") +
+                "</table>";
+            container.append(str);
+
+        } else {
+            //container.append("<p>" + "</p>");
 
         }
 
